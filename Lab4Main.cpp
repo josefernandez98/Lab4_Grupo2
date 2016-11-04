5b7805570ee72b2b2fbe25c2a960b7bc248e302d
Jose Fernandez <jrfo_04@unitec.edu>
2016-11-04 14:40:06 -0600

    Clockwise

--- Lab4Main.cpp
+++ Lab4Main.cpp
@@ -25,6 +25,24 @@ void l(int**, int**, int**, int**, int**);
 
 
 int main(int argc, char const *argv[]) {
+    int** probar = new int*[3];
+    for (int i = 0; i < 3; ++i) {
+        probar[i] = new int[3];
+    }//Fin del for
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i == 0) {
+                probar[i][j] = 1;
+            }
+            if (i == 1) {
+                probar[i][j] = 2;
+            }
+            if (i == 2) {
+                probar[i][j] = 3;
+            }
+        }
+    }
+
 
     int** frontal = new int*[3];
     for (int i = 0; i < 3; ++i) {
@@ -86,15 +104,45 @@ int main(int argc, char const *argv[]) {
         }
     }
 
-    imprimirMatriz(frontal);
-
-    limpiarMemoria(frontal);
+    imprimirMatriz(probar);
+    F(probar, frontal, frontal, frontal, frontal);
+    imprimirMatriz(probar);
+    limpiarMemoria(probar);
 
 
 
 
     return 0;
 }//Fin del main
+
+void F(int** frente, int** arriba, int** izquierda, int** derecha, int** abajo) {
+    int fila1[3];
+    int fila2[3];
+    int fila3[3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            fila1[j] = frente[0][j];
+            fila2[j] = frente[1][j];
+            fila3[j] = frente[2][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (j == 0) {
+                frente[i][j] = fila3[j];
+            }
+            if (j == 1) {
+                frente[i][j] = fila2[j];
+            }
+            if (j == 2) {
+                frente[i][j] = fila1[j];
+            }
+        }
+    }
+
+}
+
 
 void limpiarMemoria (int** matriz) {
     for (int i = 0; i < 3; ++i) {
@@ -104,7 +152,6 @@ void limpiarMemoria (int** matriz) {
     delete[] matriz;
     cout << "Matriz borrada." << endl;
 }
-
 
 void imprimirMatriz(int** matriz) {
 	for (int i = 0; i < 3; i++) {
