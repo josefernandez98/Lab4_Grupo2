11c016f494baade4b4fd8d192aaa05a417797a6d
Jose Fernandez <jrfo_04@unitec.edu>
2016-11-04 14:10:18 -0600

    fucl

--- Lab4Main.cpp
+++ Lab4Main.cpp
@@ -5,23 +5,23 @@ using namespace std;
 void imprimirMatriz(int**);
 void limpiarMemoria(int**);
 
-void F(int**, int**, int**, int**);
-void f(int**, int**, int**, int**);
+void F(int**, int**, int**, int**, int**);
+void f(int**, int**, int**, int**, int**);
 
-void X(int**, int**, int**, int**);
-void x(int**, int**, int**, int**);
+void X(int**, int**, int**, int**, int**);
+void x(int**, int**, int**, int**, int**);
 
-void U(int**, int**, int**, int**);
-void u(int**, int**, int**, int**);
+void U(int**, int**, int**, int**, int**);
+void u(int**, int**, int**, int**, int**);
 
-void B(int**, int**, int**, int**);
-void b(int**, int**, int**, int**);
+void B(int**, int**, int**, int**, int**);
+void b(int**, int**, int**, int**, int**);
 
-void R(int**, int**, int**, int**);
-void r(int**, int**, int**, int**);
+void R(int**, int**, int**, int**, int**);
+void r(int**, int**, int**, int**, int**);
 
-void L(int**, int**, int**, int**);
-void l(int**, int**, int**, int**);
+void L(int**, int**, int**, int**, int**);
+void l(int**, int**, int**, int**, int**);
 
 
 int main(int argc, char const *argv[]) {
